e84e140012c6c217e5cfc36921879ebfa8da5995
mrmitchboyer <mitch@mitchboyer.com>
2014-07-30 09:42:46 -0400

    adds bootstrap to asset pipeline

--- db/migrate/20140729181122_create_movies.rb
+++ db/migrate/20140729181122_create_movies.rb
@@ -1,12 +1,12 @@
 class CreateMovies < ActiveRecord::Migration
   def change
     create_table :movies do |t|
-    	t.string :title
-    	t.string :rating
-    	t.integer :runtime
-    	t.text :description
-    	t.string :director
-    	t.string :image_url
+      t.string :title
+      t.string :rating
+      t.integer :runtime
+      t.text :description
+      t.string :director
+      t.string :image_url
       t.timestamps
     end
   end
